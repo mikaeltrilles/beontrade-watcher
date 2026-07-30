306c2137f95960642e73e6797f2eea4c56948a6f
Mikael Trilles <mikaeltrilles@gmail.com>
2026-07-30 13:30:07 +0200

    [chore] suppression des fichiers obsolètes de Create React App
    Suppression de l'ancien code React Scripts, Redux, SCSS et composants historiques.

--- src/actions/stable.action.js
+++
@@ -1,8 +0,0 @@
-export const SET_STABLE_STATE = 'CHANGE_ETAT_STABLECOIN'; //  fonction qui permet d'afficher ou non des stablecoins
-
-
-export const setStableState = (bool) => { // fonction qui va créer l'action (ici, on a une seule action)
-  return (dispatch) => {
-    return dispatch({ type: SET_STABLE_STATE, payload: bool }); // on retourne l'action (voir src/reducers/stable.reducer.js)
-  };
-};
